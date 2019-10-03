368affa62e6a08282e7d252845d7bcd50a23c37e
filipecavalc <filipecavalc@gmail.com>
2019-10-02 21:34:55 -0300

    config acesso ao mongo atlas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:tnQKACe0QjbDduEz@cluster0-ioxlq.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(express.json());
 app.use(routes);
