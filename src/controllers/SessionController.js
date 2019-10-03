ec28cffb61437dbead6cae3226a4089d81999281
filipecavalc <filipecavalc@gmail.com>
2019-10-02 22:02:56 -0300

    cadastro usuario email

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,9 +1,14 @@
-// index, show, store, update, destroy
+const User = require('../models/User');
 
 module.exports = {
-    store(req, res) {
-        return res.json({
-            message: "teste"
-        });
+    async store(req, res) {
+        const { email } = req.body;
+
+        //const user = await User.create({ email });
+        let user = await User.findOne({ email });
+        if(!user) {
+            user = await User.create({ email });
+        }
+        return res.json(user);
     }
 };
